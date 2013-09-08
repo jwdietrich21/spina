f4618dc429d52da50102ab6ca3fd94a1961d3fc9
jwdietrich <jwdietrich@498678d1-ce32-48b4-b988-c0facfd06f0d>
2013-09-08 18:48:28 +0000

    Unit test cases for SPINA Engine created, based on FPCUnit.

---
+++ trunc/Mac OS X, Linux and Windows/SPINA Source/EngineUnitTests.lpr
@@ -0,0 +1,36 @@
+program EngineUnitTests;
+
+
+{ SPINA-Thyr }
+
+{ Application for calculating structure parameters }
+{ of thyrotropic feedback control }
+
+{ Programm zur Berechnung von Strukturparametern }
+{ des thyreotropen Regelkreises }
+
+{ Version 3.4.0 }
+
+{ (c) J. W. Dietrich, 1994 - 2013 }
+{ (c) Ludwig Maximilian University of Munich 1995 - 2002 }
+{ (c) University of Ulm Hospitals 2002-2004 }
+{ (c) Ruhr University of Bochum 2005 - 2013 }
+
+{ Test case project for PPCUnit, according to SPINA Technical Reference, Part R }
+
+{ Source code released under the BSD License }
+{ See http://spina.medical-cybernetics.de for details }
+
+{$mode objfpc}{$H+}
+
+uses
+  Interfaces, Forms, GuiTestRunner, engine_testcase, spina_engine, spina_types;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TGuiTestRunner, TestRunner);
+  Application.Run;
+end.
+
---
+++ trunc/Mac OS X, Linux and Windows/SPINA Source/engine_testcase.pas
@@ -0,0 +1,117 @@
+unit engine_testcase;
+
+{ SPINA-Thyr }
+
+{ Application for calculating structure parameters }
+{ of thyrotropic feedback control }
+
+{ Programm zur Berechnung von Strukturparametern }
+{ des thyreotropen Regelkreises }
+
+{ Version 3.4.0 }
+
+{ (c) J. W. Dietrich, 1994 - 2013 }
+{ (c) Ludwig Maximilian University of Munich 1995 - 2002 }
+{ (c) University of Ulm Hospitals 2002-2004 }
+{ (c) Ruhr University of Bochum 2005 - 2013 }
+
+{ Test case unit for PPCUnit, according to SPINA Technical Reference, Part R }
+
+{ Source code released under the BSD License }
+{ See http://spina.medical-cybernetics.de for details }
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, fpcunit, testutils, testregistry, SPINA_Types, SPINA_Engine;
+
+type
+
+  { TEngineTestCases }
+
+  TEngineTestCases= class(TTestCase)
+  published
+    procedure PositiveCheck;
+    procedure TestCase12;
+    procedure TestCase13;
+    procedure TestCase14;
+  end;
+
+var
+  testCaseRecord: tCaseRecord;
+  TSH, T4, T3: real;
+
+implementation
+
+procedure TEngineTestCases.PositiveCheck;
+begin
+  AssertNull('This test is bound to succeed', nil);
+end;
+
+procedure TEngineTestCases.TestCase12;
+  {test case #12}
+  {Normal values}
+  {TSH 1 mU/l, FT4 16.5 pmol/l, FT3 4.5 pmol/l}
+  {=> GT = 4.70 pmol/s, GD = 25.22 nmol/l}
+begin
+  gPreferences.T4.Method := freeHormone;
+  gPreferences.T3.Method := freeHormone;
+  TSH := 1;
+  T4 := 16.5e-12;
+  T3 := 4.5e-12;
+  testCaseRecord := Calculate(TSH, T4, T3);
+  AssertEquals((testCaseRecord.GT > 4.69e-12) and (testCaseRecord.GT < 4.71e-12), true);
+  AssertEquals((testCaseRecord.GD > 25.2e-9) and (testCaseRecord.GD < 25.3e-9), true);
+  AssertEquals(LeftStr(testCaseRecord.GTs, 3), '4.7');
+  AssertEquals(RightStr(testCaseRecord.GTs, 6), 'pmol/s');
+  AssertEquals(LeftStr(testCaseRecord.GDs, 4), '25.2');
+  AssertEquals(RightStr(testCaseRecord.GDs, 6), 'nmol/s');
+end;
+
+procedure TEngineTestCases.TestCase13;
+  {test case #13}
+  {T3 thyrotoxicosis}
+  {TSH 3.24 mU/l, FT4 7.7 pmol/l, FT3 28 pmol/l}
+  {=> GT = 1.08 pmol/s, GD = 336.2 nmol/l}
+begin
+  gPreferences.T4.Method := freeHormone;
+  gPreferences.T3.Method := freeHormone;
+  TSH := 3.24;
+  T4 := 7.7e-12;
+  T3 := 28e-12;
+  testCaseRecord := Calculate(TSH, T4, T3);
+  AssertEquals((testCaseRecord.GT > 1.07e-12) and (testCaseRecord.GT < 1.09e-12), true);
+  AssertEquals((testCaseRecord.GD > 336.0e-9) and (testCaseRecord.GD < 336.4e-9), true);
+  AssertEquals(LeftStr(testCaseRecord.GTs, 3), '1.0');
+  AssertEquals(RightStr(testCaseRecord.GTs, 6), 'pmol/s');
+  AssertEquals(LeftStr(testCaseRecord.GDs, 4), '336.');
+  AssertEquals(RightStr(testCaseRecord.GDs, 6), 'nmol/s');
+end;
+
+procedure TEngineTestCases.TestCase14;
+  {test case #14}
+  {Latent hyperdeiodation (hyperdeiodination)}
+  {TSH 0.7 mU/l, FT4 9 pmol/l, FT3 6.2 pmol/l}
+  {=> GT = 3.37 pmol/s, GD = 63.7 nmol/l}
+begin
+  gPreferences.T4.Method := freeHormone;
+  gPreferences.T3.Method := freeHormone;
+  TSH := 0.7;
+  T4 := 9e-12;
+  T3 := 6.2e-12;
+  testCaseRecord := Calculate(TSH, T4, T3);
+  AssertEquals((testCaseRecord.GT > 3.36e-12) and (testCaseRecord.GT < 3.38e-12), true);
+  AssertEquals((testCaseRecord.GD > 63.4e-9) and (testCaseRecord.GD < 64e-9), true);
+  AssertEquals(LeftStr(testCaseRecord.GTs, 3), '3.3');
+  AssertEquals(RightStr(testCaseRecord.GTs, 6), 'pmol/s');
+  AssertEquals(LeftStr(testCaseRecord.GDs, 4), '63.7');
+  AssertEquals(RightStr(testCaseRecord.GDs, 6), 'nmol/s');
+end;
+
+initialization
+
+  RegisterTest(TEngineTestCases);
+end.
+
