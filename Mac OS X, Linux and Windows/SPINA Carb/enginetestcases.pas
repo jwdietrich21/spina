d9cdcbf76390108957b19947ad45d42264dc83c9
Johannes W. Dietrich <65195481+jwdietrich21@users.noreply.github.com>
2024-01-14 16:55:58 +0100

    Improving precision of unit tests
    Improving precision of unit tests.

--- Mac OS X, Linux and Windows/SPINA Carb/enginetestcases.pas
+++ Mac OS X, Linux and Windows/SPINA Carb/enginetestcases.pas
@@ -64,54 +64,54 @@ procedure TEngineTestCases.TestCaseV1;
 begin
   glucose := 92 / kGlucoseConversionFactor;
   insulin := 1.2 * kInsulinConversionFactor;
-  AssertTrue(WithinTolerance(0.29, SPINA_GBeta(insulin, glucose), 0.1));
-  AssertTrue(WithinTolerance(15.83, SPINA_GR(insulin, glucose), 0.1));
+  AssertTrue(WithinTolerance(0.290, SPINA_GBeta(insulin, glucose), 0.01));
+  AssertTrue(WithinTolerance(15.855, SPINA_GR(insulin, glucose), 0.01));
   AssertEquals(SPINA_GBeta(insulin, glucose) * SPINA_GR(insulin, glucose),
                SPINA_DI(insulin, glucose));
-  AssertTrue(WithinTolerance(14.9, HOMA_Beta(insulin, glucose), 0.1));
-  AssertTrue(WithinTolerance(0.3, HOMA_IR(insulin, glucose), 0.1));
-  AssertTrue(WithinTolerance(3.67, HOMA_IS(insulin, glucose), 0.2));
-  AssertTrue(WithinTolerance(0.5, QUICKI(insulin, glucose), 0.2));
+  AssertTrue(WithinTolerance(14.897, HOMA_Beta(insulin, glucose), 0.01));
+  AssertTrue(WithinTolerance(0.273, HOMA_IR(insulin, glucose), 0.01));
+  AssertTrue(WithinTolerance(3.668, HOMA_IS(insulin, glucose), 0.2));
+  AssertTrue(WithinTolerance(0.489, QUICKI(insulin, glucose), 0.2));
 end;
 
 procedure TEngineTestCases.TestCaseV21;
 begin
   glucose := 85 / kGlucoseConversionFactor;
   insulin := 0.5 * kInsulinConversionFactor;
-  AssertTrue(WithinTolerance(0.13, SPINA_GBeta(insulin, glucose), 0.1));
-  AssertTrue(WithinTolerance(41.91, SPINA_GR(insulin, glucose), 0.1));
+  AssertTrue(WithinTolerance(0.127, SPINA_GBeta(insulin, glucose), 0.01));
+  AssertTrue(WithinTolerance(41.926, SPINA_GR(insulin, glucose), 0.01));
   AssertEquals(SPINA_GBeta(insulin, glucose) * SPINA_GR(insulin, glucose),
                SPINA_DI(insulin, glucose));
-  AssertTrue(WithinTolerance(8.2, HOMA_Beta(insulin, glucose), 0.1));
-  AssertTrue(WithinTolerance(0.1, HOMA_IR(insulin, glucose), 0.1));
-  AssertTrue(WithinTolerance(9.54, HOMA_IS(insulin, glucose), 0.2));
-  AssertTrue(WithinTolerance(0.6, QUICKI(insulin, glucose), 0.2));
+  AssertTrue(WithinTolerance(8.182, HOMA_Beta(insulin, glucose), 0.01));
+  AssertTrue(WithinTolerance(0.105, HOMA_IR(insulin, glucose), 0.01));
+  AssertTrue(WithinTolerance(9.529, HOMA_IS(insulin, glucose), 0.2));
+  AssertTrue(WithinTolerance(0.614, QUICKI(insulin, glucose), 0.2));
 end;
 
 procedure TEngineTestCases.TestCaseV30;
 begin
   glucose := 89 / kGlucoseConversionFactor;
   insulin := 10.7 * kInsulinConversionFactor;
-  AssertTrue(WithinTolerance(2.64, SPINA_GBeta(insulin, glucose), 0.1));
-  AssertTrue(WithinTolerance(1.92, SPINA_GR(insulin, glucose), 0.1));
+  AssertTrue(WithinTolerance(2.637, SPINA_GBeta(insulin, glucose), 0.01));
+  AssertTrue(WithinTolerance(1.938, SPINA_GR(insulin, glucose), 0.1));
   AssertEquals(SPINA_GBeta(insulin, glucose) * SPINA_GR(insulin, glucose),
                SPINA_DI(insulin, glucose));
-  AssertTrue(WithinTolerance(148.7, HOMA_Beta(insulin, glucose), 0.1));
-  AssertTrue(WithinTolerance(2.3, HOMA_IR(insulin, glucose), 0.1));
-  AssertTrue(WithinTolerance(0.43, HOMA_IS(insulin, glucose), 0.2));
-  AssertTrue(WithinTolerance(0.3, QUICKI(insulin, glucose), 0.2));
+  AssertTrue(WithinTolerance(148.154, HOMA_Beta(insulin, glucose), 0.01));
+  AssertTrue(WithinTolerance(2.351, HOMA_IR(insulin, glucose), 0.01));
+  AssertTrue(WithinTolerance(0.425, HOMA_IS(insulin, glucose), 0.2));
+  AssertTrue(WithinTolerance(0.336, QUICKI(insulin, glucose), 0.2));
 end;
 
 procedure TEngineTestCases.TestCaseV39;
 begin
   glucose := 108 / kGlucoseConversionFactor;
   insulin := 29.6 * kInsulinConversionFactor;
-  AssertTrue(WithinTolerance(6.54, SPINA_GBeta(insulin, glucose), 0.1));
-  AssertTrue(WithinTolerance(0.58, SPINA_GR(insulin, glucose), 0.1));
+  AssertTrue(WithinTolerance(6.54, SPINA_GBeta(insulin, glucose), 0.01));
+  AssertTrue(WithinTolerance(0.58, SPINA_GR(insulin, glucose), 0.01));
   AssertEquals(SPINA_GBeta(insulin, glucose) * SPINA_GR(insulin, glucose),
                SPINA_DI(insulin, glucose));
-  AssertTrue(WithinTolerance(237.4, HOMA_Beta(insulin, glucose), 0.1));
-  AssertTrue(WithinTolerance(7.9, HOMA_IR(insulin, glucose), 0.1));
+  AssertTrue(WithinTolerance(237.4, HOMA_Beta(insulin, glucose), 0.01));
+  AssertTrue(WithinTolerance(7.9, HOMA_IR(insulin, glucose), 0.01));
   AssertTrue(WithinTolerance(0.13, HOMA_IS(insulin, glucose), 0.2));
   AssertTrue(WithinTolerance(0.3, QUICKI(insulin, glucose), 0.2));
 end;
@@ -120,14 +120,14 @@ procedure TEngineTestCases.TestCaseV114;
 begin
   glucose := 123 / kGlucoseConversionFactor;
   insulin := 122.8 * kInsulinConversionFactor;
-  AssertTrue(WithinTolerance(25.36, SPINA_GBeta(insulin, glucose), 0.1));
-  AssertTrue(WithinTolerance(0.15, SPINA_GR(insulin, glucose), 0.1));
+  AssertTrue(WithinTolerance(25.357, SPINA_GBeta(insulin, glucose), 0.01));
+  AssertTrue(WithinTolerance(0.16, SPINA_GR(insulin, glucose), 0.1));
   AssertEquals(SPINA_GBeta(insulin, glucose) * SPINA_GR(insulin, glucose),
                SPINA_DI(insulin, glucose));
-  AssertTrue(WithinTolerance(738.3, HOMA_Beta(insulin, glucose), 0.1));
-  AssertTrue(WithinTolerance(37.3, HOMA_IR(insulin, glucose), 0.1));
-  AssertTrue(WithinTolerance(0.03, HOMA_IS(insulin, glucose), 0.2));
-  AssertTrue(WithinTolerance(0.2, QUICKI(insulin, glucose), 0.2));
+  AssertTrue(WithinTolerance(736.800, HOMA_Beta(insulin, glucose), 0.01));
+  AssertTrue(WithinTolerance(37.295, HOMA_IR(insulin, glucose), 0.01));
+  AssertTrue(WithinTolerance(0.027, HOMA_IS(insulin, glucose), 0.2));
+  AssertTrue(WithinTolerance(0.239, QUICKI(insulin, glucose), 0.2));
 end;
 
 procedure TControlTestCases.PositiveCheck;
