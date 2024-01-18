cb39458e49c62544cd625ad684bd2fe629f35c1a
Johannes W. Dietrich <65195481+jwdietrich21@users.noreply.github.com>
2024-01-18 21:12:29 +0100

    Implementing a first version of functions for SPINA Carb in Python
    Implementing a first version of functions for SPINA Carb in Python.

---
+++ Python functions/spina_functions.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+# SPINA Carb
+
+# Software for calculating structure parameters
+# of insulin-glucose feedback control
+
+# Version 5.1.0 (Cyclone)
+
+# (c) J. W. Dietrich, 1994 - 2024 
+# (c) Ludwig Maximilian University of Munich 1995 - 2002 
+# (c) University of Ulm Hospitals 2002 - 2004 
+# (c) Ruhr University of Bochum 2005 - 2024 
+
+# This unit implements the calculation engine 
+
+# Source code released under the BSD License 
+# See http://spina.medical-cybernetics.de for details  
+
+import math
+
+INSULINCONVERSIONFACTOR = 6;
+GLUCOSECONVERSIONFACTOR = 18;
+PICOFACTOR = 1E12;
+MILLIFACTOR = 1E3;
+BETAI = 3.4E-3;
+ALPHAI = 0.2;
+DBETA = 7E-3;
+ALPHAG = 0.11;
+BETAG = 7.1E-4; 
+P0 = 150e-6;
+DR = 1.6e-9;
+GE = 50;
+P1 = 22.5;
+P2 = 20;
+P3 = 3.5;
+kError101 = 'Runtime error: Negative parameters';
+kError102 = 'Runtime error: Parameter out of range';
+
+
+def SPINA_GBeta(Insulin, Glucose):
+  result = float("NaN")
+  if math.isnan(Insulin) or math.isnan(Glucose):
+    result = float("NaN")
+  else:
+    result = PICOFACTOR * BETAI * Insulin / PICOFACTOR * (DBETA + Glucose / MILLIFACTOR) / (ALPHAI * Glucose / MILLIFACTOR)
+  return result
+
+def SPINA_GR(Insulin, Glucose):
+  result = float("NaN")
+  if math.isnan(Insulin) or math.isnan(Glucose):
+    result = float("NaN")
+  else:
+    result = ALPHAG * P0 * (DR + Insulin / PICOFACTOR) / (BETAG * GE * Insulin / PICOFACTOR * Glucose / MILLIFACTOR) - DR / (GE * Insulin / PICOFACTOR) - 1 / GE
+  return result
+
+def SPINA_DI(Insulin, Glucose):
+  result = float("NaN")
+  if math.isnan(Insulin) or math.isnan(Glucose):
+    result = float("NaN")
+  else:
+    result = SPINA_GBeta(Insulin, Glucose) * SPINA_GR(Insulin, Glucose)
+  return result
+
+def HOMA_Beta(Insulin, Glucose):
+  result = float("NaN")
+  if math.isnan(Insulin) or math.isnan(Glucose):
+    result = float("NaN")
+  else:
+    result = P2 * Insulin / INSULINCONVERSIONFACTOR / (Glucose - P3)
+  return result
+
+def HOMA_IR(Insulin, Glucose):
+  result = float("NaN")
+  if math.isnan(Insulin) or math.isnan(Glucose):
+    result = float("NaN")
+  else:
+    result = Glucose * Insulin / INSULINCONVERSIONFACTOR / P1
+  return result
+
+def HOMA_IS(Insulin, Glucose):
+  result = float("NaN")
+  if math.isnan(Insulin) or math.isnan(Glucose):
+    result = float("NaN")
+  else:
+    result = 1 / HOMA_IR(Insulin, Glucose)
+  return result
+
+def QUICKI(Insulin, Glucose):
+  result = float("NaN")
+  if math.isnan(Insulin) or math.isnan(Glucose):
+    result = float("NaN")
+  else:
+    result = 1 / (math.log10(Insulin / INSULINCONVERSIONFACTOR) + math.log10(Glucose * GLUCOSECONVERSIONFACTOR))
+  return result
+
+
+Glucose = 92 / GLUCOSECONVERSIONFACTOR
+Insulin = 1.2 * INSULINCONVERSIONFACTOR
+print('SPINA-GBeta:', SPINA_GBeta(Insulin, Glucose))  
+print('SPINA-GR:', SPINA_GR(Insulin, Glucose))  
+print('SPINA-DI:', SPINA_DI(Insulin, Glucose))  
+print('HOMA-Beta:', HOMA_Beta(Insulin, Glucose))  
+print('HOMA-IR:', HOMA_IR(Insulin, Glucose))  
+print('HOMA-IS:', HOMA_IS(Insulin, Glucose))  
+print('QUICKI', QUICKI(Insulin, Glucose))  
+
