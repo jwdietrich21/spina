11b9bb3ae81934024ef3a98465eebf51740b7143
Johannes W. Dietrich <65195481+jwdietrich21@users.noreply.github.com>
2024-02-17 23:55:44 +0100

    Expanding the GUI of SPINA Carb
    Expanding the GUI of SPINA Carb.

--- Mac OS X, Linux and Windows/SPINA Carb/gui.pas
+++ Mac OS X, Linux and Windows/SPINA Carb/gui.pas
@@ -26,30 +26,61 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  Spin;
+  Spin, Menus, ActnList, StdActns, Math, LCLType;
 
 type
 
   { THauptschirm }
 
   THauptschirm = class(TForm)
+    ActionList1: TActionList;
+    AppleMenu: TMenuItem;
     CalculateButton: TButton;
+    CloseMenuItem: TMenuItem;
+    CopyMenuItem: TMenuItem;
+    CutMenuItem: TMenuItem;
+    Divider11: TMenuItem;
+    Divider12: TMenuItem;
+    Divider21: TMenuItem;
+    Divider22: TMenuItem;
+    EditCopy1: TEditCopy;
+    EditMenu: TMenuItem;
+    EditSelectAll1: TEditSelectAll;
+    FileMenu: TMenuItem;
     GlucoseUnitsCombo: TComboBox;
     GlucoseEdit: TEdit;
     EntryBox: TGroupBox;
     FeedbackImage: TImage;
+    HelpMenu: TMenuItem;
     InsulinUnitsCombo: TComboBox;
     InsulinEdit: TEdit;
     GlucoseLabel: TLabel;
     InsulinLabel: TLabel;
+    MacAboutItem: TMenuItem;
+    MainMenu1: TMainMenu;
+    NewMenuItem: TMenuItem;
+    OpenMenuItem: TMenuItem;
+    PasteMenuItem: TMenuItem;
+    QuitMenuItem: TMenuItem;
+    RedoMenuItem: TMenuItem;
     ResultsBox: TGroupBox;
     HintBox: TGroupBox;
     Memo1: TMemo;
     Panel1: TPanel;
+    SaveMenuItem: TMenuItem;
+    SelectAllMenuItem: TMenuItem;
+    UndoMenuItem: TMenuItem;
+    WinAboutItem: TMenuItem;
+    procedure CalculateButtonClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure QuitMenuItemClick(Sender: TObject);
   private
 
   public
-
+    InsulinRaw, GlucoseRaw: extended;
+    InsulinUoM, GlucoseUoM: String;
+    procedure RegisterEntry(Sender: TObject);
+    procedure AdaptMenus;
   end;
 
 var
@@ -58,6 +89,65 @@ var
 implementation
 
 {$R *.lfm}
+
+{ THauptschirm }
+
+procedure THauptschirm.CalculateButtonClick(Sender: TObject);
+begin
+  RegisterEntry(Sender);
+end;
+
+procedure THauptschirm.FormCreate(Sender: TObject);
+begin
+  AdaptMenus;
+end;
+
+procedure THauptschirm.QuitMenuItemClick(Sender: TObject);
+begin
+  application.Terminate;
+end;
+
+procedure THauptschirm.RegisterEntry(Sender: TObject);
+begin
+  InsulinRaw := StrToFloatDef(InsulinEdit.Text, Math.Nan);
+  GlucoseRaw := StrToFloatDef(GlucoseEdit.Text, Math.Nan);
+  InsulinUoM := InsulinUnitsCombo.Text;
+  GlucoseUoM := GlucoseUnitsCombo.Text;
+end;
+
+procedure THauptschirm.AdaptMenus;
+{ Adapts Menus and Shortcuts to the interface style guidelines
+  of the respective operating system }
+var
+  modifierKey: TShiftState;
+begin
+  {$IFDEF LCLcarbon}
+  modifierKey := [ssMeta];
+  WinAboutItem.Visible := False;
+  AppleMenu.Visible := True;
+  {$ELSE}
+  {$IFDEF LCLCocoa}
+  modifierKey := [ssMeta];
+  WinAboutItem.Visible := False;
+  AppleMenu.Visible := True;
+  {$ELSE}
+  modifierKey := [ssCtrl];
+  WinAboutItem.Visible := True;
+  AppleMenu.Visible := False;
+  {$ENDIF}
+  {$ENDIF}
+  NewMenuItem.ShortCut := ShortCut(VK_N, modifierKey);
+  OpenMenuItem.ShortCut := ShortCut(VK_O, modifierKey);
+  CloseMenuItem.ShortCut := ShortCut(VK_W, modifierKey);
+  SaveMenuItem.ShortCut := ShortCut(VK_S, modifierKey);
+  QuitMenuItem.ShortCut := ShortCut(VK_Q, modifierKey);
+  UndoMenuItem.ShortCut := ShortCut(VK_Z, modifierKey);
+  RedoMenuItem.ShortCut := ShortCut(VK_Z, modifierKey + [ssShift]);
+  CutMenuItem.ShortCut := ShortCut(VK_X, modifierKey);
+  CopyMenuItem.ShortCut := ShortCut(VK_C, modifierKey);
+  PasteMenuItem.ShortCut := ShortCut(VK_V, modifierKey);
+  SelectAllMenuItem.ShortCut := ShortCut(VK_A, modifierKey);
+end;
 
 end.
 
