d84160e0ed7752f2e285b9eb3dbdb5cba869c055
Johannes W. Dietrich <65195481+jwdietrich21@users.noreply.github.com>
2026-07-11 22:18:50 +0200

    SPINA R Package: Implementing testing environment and writing tests for SPINA Thyr
    SPINA R Package: Implementing testing environment and writing tests for SPINA Thyr.

---
+++ S functions/R Package/SPINA/tests/testthat/test-spina_functions.R
@@ -0,0 +1,48 @@
+test_that("Scenario 9: SPINA-GT = 4.7 pmol/s, SPINA-GD = 25.22 nmol/s",
+{
+  TSH <- 1;
+  FT4 <- 16.5;
+  FT3 <- 4.5;
+  expect_lt(abs(SPINA.GT(TSH, FT4) - 4.7), 0.05);   # Result rounded
+  expect_lt(abs(SPINA.GD(FT4, FT3) - 25.22), 0.05); # Difference should be less than 0.05
+  expect_lt(abs(estimated.TSHI(TSH, FT4) - 2.21), 0.05);
+  expect_lt(abs(estimated.TTSI(TSH, FT4, 18) - 91.7), 0.05);
+})
+
+test_that("Scenario 10: SPINA-GT = 1.08 pmol/s, SPINA-GD = 336.2 nmol/s",
+{
+  TSH <- 3.24;
+  FT4 <- 7.7;
+  FT3 <- 28;
+  expect_lt(abs(SPINA.GT(TSH, FT4) - 1.08), 0.05);   # Result rounded
+  expect_lt(abs(SPINA.GD(FT4, FT3) - 336.2), 0.05);  # Difference should be less than 0.05
+})
+
+test_that("Scenario 11: SPINA-GT = 3.37 pmol/s, SPINA-GD = 63.7 nmol/s",
+{
+  TSH <- 0.7;
+  FT4 <- 9;
+  FT3 <- 6.2;
+  expect_lt(abs(SPINA.GT(TSH, FT4) - 3.37), 0.05);   # Result rounded
+  expect_lt(abs(SPINA.GD(FT4, FT3) - 63.7), 0.05);   # Difference should be less than 0.05
+})
+
+test_that("Scenarios 9 to 11 in vector form, length 3",
+{
+  TSH <- c(1, 3.24, 0.7);
+  FT4 <- c(16.5, 7.7, 9);
+  FT3 <- c(4.5, 28, 6.2);
+  expect_length(SPINA.GT(TSH, FT4), 3);
+  expect_length(SPINA.GD(FT4, FT3), 3)
+})
+
+test_that("Scenario 13: SPINA-GT = 5.73 pmol/s, SPINA-GD = 36.12 nmol/s, TSHI = -0.91, TTSI = 4",
+{
+  TSH <- 0.2;
+  FT4 <- 5.12;
+  FT3 <- 2;
+  expect_lt(abs(SPINA.GT(TSH, FT4) - 5.73), 0.05);   # Result rounded
+  expect_lt(abs(SPINA.GD(FT4, FT3) - 36.12), 0.05);  # Difference should be less than 0.05
+  expect_lt(abs(estimated.TSHI(TSH, FT4) - -0.91), 0.05);
+  expect_lt(abs(estimated.TTSI(TSH, FT4, 18) - 5.7), 0.05);
+})
