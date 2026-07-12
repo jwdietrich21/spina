2ff3c4f106cacb2f42147da105e7b9177e8cd010
Johannes W. Dietrich <65195481+jwdietrich21@users.noreply.github.com>
2026-07-12 18:46:02 +0200

    SPINA R Package: Adding tests for SPINA Carb and updating meta-data
    SPINA R Package: Adding tests for SPINA Carb and updating meta-data.

--- S functions/R Package/SPINA/tests/testthat/test-spina_functions.R
+++ S functions/R Package/SPINA/tests/testthat/test-spina_functions.R
@@ -46,3 +46,39 @@ test_that("Scenario 13: SPINA-GT = 5.73 pmol/s, SPINA-GD = 36.12 nmol/s, TSHI = 
   expect_lt(abs(estimated.TSHI(TSH, FT4) - -0.91), 0.05);
   expect_lt(abs(estimated.TTSI(TSH, FT4, 18) - 5.7), 0.05);
 })
+
+test_that("Scenario 100: SPINA-GBeta = 2.8 pmol/s, SPINA-GR = 2.3 mol/s",
+{
+  Insulin <- 63.01;
+  Glucose <- 4.34;
+  expect_lt(abs(SPINA.GBeta(Insulin, Glucose) - 2.8), 0.05);   # Result rounded
+  expect_lt(abs(SPINA.GR(Insulin, Glucose) - 2.3), 0.05);  # Difference should be less than 0.05
+  expect_lt(abs(SPINA.DI(Insulin, Glucose) - 2.8 * 2.3), 0.09);
+})
+
+test_that("Scenario 101: SPINA-GBeta = 2.8 pmol/s, SPINA-GR = 0.7 mol/s",
+{
+  Insulin <- 88.77;
+  Glucose <- 8.18;
+  expect_lt(abs(SPINA.GBeta(Insulin, Glucose) - 2.8), 0.05);   # Result rounded
+  expect_lt(abs(SPINA.GR(Insulin, Glucose) - 0.7), 0.05);  # Difference should be less than 0.05
+  expect_lt(abs(SPINA.DI(Insulin, Glucose) - 2.8 * 0.7), 0.09);
+})
+
+test_that("Scenario 102: SPINA-GBeta = 0.6 pmol/s, SPINA-GR = 2.3 mol/s",
+{
+  Insulin <- 20.33;
+  Glucose <- 9.51;
+  expect_lt(abs(SPINA.GBeta(Insulin, Glucose) - 0.6), 0.05);   # Result rounded
+  expect_lt(abs(SPINA.GR(Insulin, Glucose) - 2.3), 0.05);  # Difference should be less than 0.05
+  expect_lt(abs(SPINA.DI(Insulin, Glucose) - 0.6 * 2.3), 0.09);
+})
+
+test_that("Scenarios 100 to 102 in vector form, length 3",
+{
+  Insulin <- c(63.01, 88.77, 20.33);
+  Glucose <- c(4.34, 8.18, 9.51);
+  expect_length(SPINA.GBeta(Insulin, Glucose), 3);
+  expect_length(SPINA.GR(Insulin, Glucose), 3);
+  expect_length(SPINA.DI(Insulin, Glucose), 3);
+})
